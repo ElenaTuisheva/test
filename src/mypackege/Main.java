99fd36e8c08dc283511342899a8a995acd6b06e6
ElenaTuisheva <lenka-a-a-a-a@yandex.ru>
2021-03-23 14:07:20 +0300

    1 и 2 задание

--- src/mypackege/Main.java
+++ src/mypackege/Main.java
@@ -2,8 +2,26 @@ package mypackege;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "Hello World";
-        System.out.println(s);
+        System.out.println("1 задание");
+        String line = "Шаурмяу";
+        System.out.println("до: " + line);
+        line = line.substring(0,5);
+        String lineA = "а";
+        line = line.concat(lineA);
+        System.out.println("после: " + line);
+        System.out.println("2 задание");
+        String s1 = "Много $";
+        System.out.println("до: " + s1);
+        String s2 = s1.replace("$", "&");
+        System.out.println("после: " + s2);
+        System.out.println("3 задание");
+        String NG = "осталось много времени до нового года";
 
+
+
+//        3) "осталось много времени до нового года" - превратите в "осталось 4 месяца до нового года".
+//        4) превратите из предыдущей строки количество месяцев в цифру. Уменьшите ее на единицу. выведите.
+//          задание со звездочкой: 5) задание со звездочкой: превратить "сосед и децибелы" в "соседи имбецилы"   =). работой со строкой, естественно.
+//          покажите работу с методами substring, parse, trim
     }
 }
