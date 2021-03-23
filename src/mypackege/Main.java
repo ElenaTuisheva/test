db8034f541f9461436ae03d620a3e02fc35db4f7
ElenaTuisheva <lenka-a-a-a-a@yandex.ru>
2021-03-23 20:08:45 +0300

    4, 5 задание

--- src/mypackege/Main.java
+++ src/mypackege/Main.java
@@ -2,28 +2,37 @@ package mypackege;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1 задание"); //превратить строку "Шаурмяу" в строку "Шаурма"
+        System.out.println(); //превратить строку "Шаурмяу" в строку "Шаурма"
         String line = "Шаурмяу";
         System.out.println("до: " + line);
         line = line.substring(0,5);
         String lineA = "а";
         line = line.concat(lineA);
         System.out.println("после: " + line);
-        System.out.println("2 задание"); //"Много $" превратить в "Много &"
+
+        System.out.println(); //"Много $" превратить в "Много &"
         String s1 = "Много $";
         System.out.println("до: " + s1);
         String s2 = s1.replace("$", "&");
         System.out.println("после: " + s2);
-        System.out.println("3 задание"); //"осталось много времени до нового года" - превратите в "осталось 4 месяца до нового года".
+
+        System.out.println(); //"осталось много времени до нового года" - превратите в "осталось 4 месяца до нового года".
         String NG = "осталось много времени до нового года";
         System.out.println(NG);
-        System.out.println(NG.replace("много времени", "4 месяца"));
+        NG = NG.replace("много времени", "4 месяца");
+        System.out.println(NG);
 
+        System.out.println();//превратите из предыдущей строки количество месяцев в цифру. Уменьшите ее на единицу. выведите.
+        String[] array = NG.split("\\s");
+        Integer i = Integer.parseInt(array[1]);
+        i-=1;
+        String m = Integer.toString(i);
+        System.out.println(array[0] + " " + m + " " + array[2] + " " + array[3] + " " + array[4] + " " + array[5]);
+        System.out.println();//задание со звездочкой: превратить "сосед и децибелы" в "соседи имбецилы"
+        String sosed = "сосед и децибелы";
+        System.out.println(sosed);
+        sosed = sosed.replace(" и децибе", "и имбеци");
+        System.out.println(sosed);
 
-
-
-//        4) превратите из предыдущей строки количество месяцев в цифру. Уменьшите ее на единицу. выведите.
-//          задание со звездочкой: 5) задание со звездочкой: превратить "сосед и децибелы" в "соседи имбецилы"   =). работой со строкой, естественно.
-//          покажите работу с методами substring, parse, trim
     }
 }
